57ffff28eeef7d46607602d2c58b1c29ad83bf1e
danielphilippi <daniel2.philippi@gmail.com>
2021-03-27 22:06:21 +0100

    refactoring country and city codes continued

--- datamanager/dataloader.py
+++ datamanager/dataloader.py
@@ -10,23 +10,23 @@ rawdatainfo = pd.read_csv(joinpath(DATAMGR_PATH, 'overview_rawdata.csv'), sep=';
 
 # city codes
 if not os.path.isfile(UNLOC_FILE_CLEAN_ABS):
-    unloc = prepare_unloc(unloc_files=UNLOC_FILES_ABS)
+    city_codes = prepare_unloc(unloc_files=UNLOC_FILES_ABS)
     with open(UNLOC_FILE_CLEAN_ABS, 'wb') as f:
-        pickle.dump(unloc, f)
+        pickle.dump(city_codes, f)
 else:
     with open(UNLOC_FILE_CLEAN_ABS, 'rb') as f:
-        unloc = pickle.load(f)
-    print(unloc.shape)
+        city_codes = pickle.load(f)
+    print(city_codes.shape)
 
 # ccodes
 if not os.path.isfile(CCODE_FILE_CLEAN_ABS):
-    ccodes = prepare_ccodes(ccode_file=CCODE_FILE_ABS)
+    country_codes = prepare_ccodes(ccode_file=CCODE_FILE_ABS)
     with open(CCODE_FILE_CLEAN_ABS, 'wb') as f:
-        pickle.dump(ccodes, f)
+        pickle.dump(country_codes, f)
 else:
     with open(CCODE_FILE_CLEAN_ABS, 'rb') as f:
-        ccodes = pickle.load(f)
-    print(unloc.shape)
+        country_codes = pickle.load(f)
+    print(country_codes.shape)
 
 
-check_ccode_loccode(ccodes, unloc)
+check_ccode_loccode(country_codes, city_codes)
